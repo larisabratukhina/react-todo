d78a2553d689c8c9d8d8034a1384827ca7d96d2c
Lara Bee <larisa.bratukhina@gmail.com>
2023-10-09 10:49:15 -0700

    Create Todo List (#1)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
+const todoList = [
+  { id: 1, title: 'Read the requirements' },
+  { id: 2, title: 'Complete assignment' },
+  { id: 3, title: 'Submit assignment' }
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
